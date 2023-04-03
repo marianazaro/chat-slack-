1212275d7321cdc577232b289d490c46a4647691
maria <marianazarova892@gmail.com>
2023-04-03 16:23:31 +0200

    changed the size of the image

--- frontend/src/NotFound.js
+++ frontend/src/NotFound.js
@@ -1,20 +1,13 @@
 const NotFound = () => (
   <div className="h-100">
-    <div className="h-100" id="chat">
-      <div className="d-flex flex-column h-100">
-        <nav className="shadow-sm navbar navbar-expend-lg navbar-light bg-white">
-          <div className="container">
-            <a className="navbar-brand" href="/">Hexlet Chat</a>
-          </div>
-        </nav>
-        <div className="text-center">
-          <img alt="Страница не найдена" className="img-fluid h-25" src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
-          <h1 className="h4 text-muted">Страница не найдена</h1>
-          <p className="text-muted">
-            Но вы можете перейти
-            <a href="/">на главную страницу</a>
-          </p>
-        </div>
+    <div className="d-flex flex-column h-100">
+      <div className="text-center">
+        <img alt="Страница не найдена" className="img-fluid col-sm-3" src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
+        <h1 className="h4 text-muted">Страница не найдена</h1>
+        <p className="text-muted">
+          Но вы можете перейти
+          <a href="/">на главную страницу</a>
+        </p>
       </div>
     </div>
   </div>
