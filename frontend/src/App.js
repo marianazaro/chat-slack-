0536611d171fbabee0dfca158c6070e87443fc28
maria <marianazarova892@gmail.com>
2023-04-03 15:48:07 +0200

    configuring routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './LoginForm.js';
+import NotFound from './NotFound.js';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<LoginForm />} />
+      <Route path="login" element={<LoginForm />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
